ea2fef46214d6518c26e5e60558603571f18f977
mohsin604 <mohsinshahid604@gmai.com>
2022-11-04 14:53:30 +0500

    add

--- configration/variable.js
+++ configration/variable.js
@@ -1,6 +1,6 @@
 const dotenv=require('dotenv')
 let MyEnviroment={}
 if(process.env.NODE_ENV ==='testing'){
-MyEnviroment =dotenv.config({path:`${__dirname}/../testing.env`});
+MyEnviroment =dotenv.config({path:`${__dirname}/../tasting.env`});
 }
 console.log(MyEnviroment)
